e504a8d827be633349cc355b9cff7c936335e5d1
AFKAPP <96723081+afkapp@users.noreply.github.com>
2022-01-17 06:32:24 +0000

    Forever.py for Windows OS and Linux OS

--- src/process/forever.py
+++ src/process/forever.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 from subprocess import Popen
 from colorama import init, Fore
+import sys
 init()
 
 while True:
-    print(Fore.MAGENTA + 'Starting bot with infinite execution!' + Fore.RESET)
-    p = Popen("python .\index.py", shell=True)
+    print(Fore.MAGENTA + 'Starting the bot with loop execution...' + Fore.RESET)
+    if sys.platform != 'linux' and sys.platform != 'linux2':
+        p = Popen("python .\index.py", shell=True)
+    if sys.platform == 'linux' or sys.platform == 'linux2':
+        p = Popen("python3 .\index.py", shell=True)
+
     p.wait() 
